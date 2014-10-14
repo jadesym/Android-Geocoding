eb5876c0dcb291f7bde004ea3895408fc293a140
Kevin <kevin.fu808@gmail.com>
2014-10-14 18:37:18 -0500

    first commit

---
+++ gen/edu/utexas/shenfu/mapsapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.utexas.shenfu.mapsapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/edu/utexas/shenfu/mapsapp/MainActivity.java
@@ -0,0 +1,83 @@
+package edu.utexas.shenfu.mapsapp;
+
+import com.google.android.gms.maps.GoogleMap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.maps.MapFragment;
+
+
+public class MainActivity extends Activity {
+	
+	private GoogleMap googleMap;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		 try {
+	            // Loading map
+	            initilizeMap();
+	    
+		 } catch (Exception e) {
+	            e.printStackTrace();
+	        
+	        
+	    }
+	}
+	
+	/**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+ 
+    
+    public void onClick (View view) {
+		EditText text = (EditText) findViewById(R.id.main_input);
+		Toast.makeText(this, text.getText().toString(),
+		Toast.LENGTH_LONG).show();
+		  
+	} 
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
